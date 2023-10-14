4a8c862d8d835f0ea90626f0d3c28c5e3ec1bbc2
nicolas gomez morgado <nicolas.gomez2101@alumnos.ubiobio.cl>
2023-10-14 13:54:40 -0300

    guardar tiempos 	modified:   P_1_M_2.c

--- P_1_M_2.c
+++ P_1_M_2.c
@@ -12,143 +12,233 @@ void sumarMatrices(int **A, int **B, int **C, int n) {
 
 void multiplicarMatrices(short **A, short **B, int **C, int n) {
     if (n == 1) {
-        C[0][0] = (int)(A[0][0] * B[0][0]);
+        // Caso base: matrices de 1x1
+        C[0][0] = (int)A[0][0] * B[0][0];
     } else {
+        // Dividir las matrices en submatrices
         int m = n / 2;
-        short ***submatrices = malloc(6 * sizeof(short **));
 
-        for (int i = 0; i < 6; i++) {
-            submatrices[i] = malloc(m * sizeof(short *));
+        // Crear submatrices de A, B y C
+        short **A11, **A12, **A21, **A22;
+        short **B11, **B12, **B21, **B22;
+        int **C11, **C12, **C21, **C22;
+        int **Temp1, **Temp2;
+
+        A11 = (short **)malloc(m * sizeof(short *));
+        A12 = (short **)malloc(m * sizeof(short *));
+        A21 = (short **)malloc(m * sizeof(short *));
+        A22 = (short **)malloc(m * sizeof(short *));
+        B11 = (short **)malloc(m * sizeof(short *));
+        B12 = (short **)malloc(m * sizeof(short *));
+        B21 = (short **)malloc(m * sizeof(short *));
+        B22 = (short **)malloc(m * sizeof(short *));
+        C11 = (int **)malloc(m * sizeof(int *));
+        C12 = (int **)malloc(m * sizeof(int *));
+        C21 = (int **)malloc(m * sizeof(int *));
+        C22 = (int **)malloc(m * sizeof(int *));
+        Temp1 = (int **)malloc(m * sizeof(int *));
+        Temp2 = (int **)malloc(m * sizeof(int *));
+
+        for (int i = 0; i < m; i++) {
+            A11[i] = (short *)malloc(m * sizeof(short));
+            A12[i] = (short *)malloc(m * sizeof(short));
+            A21[i] = (short *)malloc(m * sizeof(short));
+            A22[i] = (short *)malloc(m * sizeof(short));
+            B11[i] = (short *)malloc(m * sizeof(short));
+            B12[i] = (short *)malloc(m * sizeof(short));
+            B21[i] = (short *)malloc(m * sizeof(short));
+            B22[i] = (short *)malloc(m * sizeof(short));
+            C11[i] = (int *)malloc(m * sizeof(int));
+            C12[i] = (int *)malloc(m * sizeof(int));
+            C21[i] = (int *)malloc(m * sizeof(int));
+            C22[i] = (int *)malloc(m * sizeof(int));
+            Temp1[i] = (int *)malloc(m * sizeof(int));
+            Temp2[i] = (int *)malloc(m * sizeof(int));
+        }
+
+        // Dividir A en submatrices
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
-                submatrices[i][j] = malloc(m * sizeof(short));
+                A11[i][j] = A[i][j];
+                A12[i][j] = A[i][j + m];
+                A21[i][j] = A[i + m][j];
+                A22[i][j] = A[i + m][j + m];
             }
         }
 
-        // Dividir A y B en submatrices
+        // Dividir B en submatrices
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
-                submatrices[0][i][j] = A[i][j];
-                submatrices[1][i][j] = A[i][j + m];
-                submatrices[2][i][j] = A[i + m][j];
-                submatrices[3][i][j] = A[i + m][j + m];
-
-                submatrices[4][i][j] = B[i][j];
-                submatrices[5][i][j] = B[i][j + m];
+                B11[i][j] = B[i][j];
+                B12[i][j] = B[i][j + m];
+                B21[i][j] = B[i + m][j];
+                B22[i][j] = B[i + m][j + m];
             }
         }
 
         // Realizar las operaciones de multiplicación de forma recursiva
-        int ***Temp = malloc(4 * sizeof(int **));
-        for (int i = 0; i < 4; i++) {
-            Temp[i] = malloc(m * sizeof(int *));
+        multiplicarMatrices(A11, B11, Temp1, m);
+        multiplicarMatrices(A12, B21, Temp2, m);
+        sumarMatrices(Temp1, Temp2, C11, m);
+
+        multiplicarMatrices(A11, B12, Temp1, m);
+        multiplicarMatrices(A12, B22, Temp2, m);
+        sumarMatrices(Temp1, Temp2, C12, m);
+
+        multiplicarMatrices(A21, B11, Temp1, m);
+        multiplicarMatrices(A22, B21, Temp2, m);
+        sumarMatrices(Temp1, Temp2, C21, m);
+
+        multiplicarMatrices(A21, B12, Temp1, m);
+        multiplicarMatrices(A22, B22, Temp2, m);
+        sumarMatrices(Temp1, Temp2, C22, m);
+
+        // Combinar las submatrices para obtener C
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
-                Temp[i][j] = malloc(m * sizeof(int));
+                C[i][j] = C11[i][j];
+                C[i][j + m] = C12[i][j];
+                C[i + m][j] = C21[i][j];
+                C[i + m][j + m] = C22[i][j];
             }
         }
 
-        multiplicarMatrices(submatrices[0], submatrices[4], Temp[0], m);
-        multiplicarMatrices(submatrices[1], submatrices[5], Temp[1], m);
-        sumarMatrices(Temp[0], Temp[1], C, m);
-
-        multiplicarMatrices(submatrices[2], submatrices[4], Temp[2], m);
-        multiplicarMatrices(submatrices[3], submatrices[5], Temp[3], m);
-        sumarMatrices(Temp[2], Temp[3], C + m, m);
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < m; j++) {
-                free(Temp[i][j]);
-            }
-            free(Temp[i]);
+        for (int i = 0; i < m; i++) {
+            free(A11[i]);
+            free(A12[i]);
+            free(A21[i]);
+            free(A22[i]);
+            free(B11[i]);
+            free(B12[i]);
+            free(B21[i]);
+            free(B22[i]);
+            free(C11[i]);
+            free(C12[i]);
+            free(C21[i]);
+            free(C22[i]);
+            free(Temp1[i]);
+            free(Temp2[i]);
         }
-        free(Temp);
-
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < m; j++) {
-                free(submatrices[i][j]);
-            }
-            free(submatrices[i]);
-        }
-        free(submatrices);
+        free(A11);
+        free(A12);
+        free(A21);
+        free(A22);
+        free(B11);
+        free(B12);
+        free(B21);
+        free(B22);
+        free(C11);
+        free(C12);
+        free(C21);
+        free(C22);
+        free(Temp1);
+        free(Temp2);
     }
+}
+
+void guardarTiemposEnArchivo(double tiempo, char* nombreArchivo,int sizeMatriz) {
+    FILE* archivo = fopen(nombreArchivo, "a");
+    if (archivo == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
+    fprintf(archivo, "Tiempo en segundos para matriz de %dx%d = {%.6f}\n", sizeMatriz, sizeMatriz, tiempo/10);
+    fclose(archivo);
 }
 
 int main() {
     clock_t start, end;
     double tiempo;
 
-    int sizeMatriz = 2;
+    int sizeMatriz = 32;
 
-    short **Matriz1 = malloc(sizeMatriz * sizeof(short *));
-    short **Matriz2 = malloc(sizeMatriz * sizeof(short *));
-    int **Matriz3 = malloc(sizeMatriz * sizeof(int *));
+    do {
+        double promedio = 0;
+        int rep = 1;
 
-    for (int i = 0; i < sizeMatriz; i++) {
-        Matriz1[i] = malloc(sizeMatriz * sizeof(short));
-        Matriz2[i] = malloc(sizeMatriz * sizeof(short));
-        Matriz3[i] = malloc(sizeMatriz * sizeof(int));
-    }
+        do {
+            short **Matriz1 = (short **)malloc(sizeMatriz * sizeof(short *));
+            short **Matriz2 = (short **)malloc(sizeMatriz * sizeof(short *));
+            int **Matriz3 = (int **)malloc(sizeMatriz * sizeof(int *));
 
-    // Inicializar Matriz1 y Matriz2 con valores aleatorios
-    srand(time(NULL));
-    for (int i = 0; i < sizeMatriz; i++) {
-        for (int j = 0; j < sizeMatriz; j++) {
-            Matriz1[i][j] = rand() % 128 + 0;
-            Matriz2[i][j] = rand() % 128 + 0;
-        }
-    }
-
-    int c = 0;
-    for (int i = 0; i < sizeMatriz; i++) {
-        for (int j = 0; j < sizeMatriz; j++) {
-            printf("<%d>",Matriz1[i][j]);
-            c++;
-            if (c == sizeMatriz)
-            {
-                printf("\n");
-                c = 0;
+            for (int i = 0; i < sizeMatriz; i++) {
+                Matriz1[i] = (short *)malloc(sizeMatriz * sizeof(short));
+                Matriz2[i] = (short *)malloc(sizeMatriz * sizeof(short));
+                Matriz3[i] = (int *)malloc(sizeMatriz * sizeof(int));
             }
-        }
-    }
-    c = 0;
-    for (int i = 0; i < sizeMatriz; i++) {
-        for (int j = 0; j < sizeMatriz; j++) {
-            printf("{%d}",Matriz2[i][j]);
-            c++;
-            if (c == sizeMatriz)
-            {
-                printf("\n");
-                c = 0;
+
+            // Inicializar Matriz1 y Matriz2 con valores aleatorios
+            for (int i = 0; i < sizeMatriz; i++) {
+                for (int j = 0; j < sizeMatriz; j++) {
+                    Matriz1[i][j] = rand() % 128 + 0;
+                    Matriz2[i][j] = rand() % 128 + 0;
+                }
+
             }
-        }
-    }
 
-    start = clock();
-    multiplicarMatrices(Matriz1, Matriz2, Matriz3, sizeMatriz);
-    end = clock();
-    c = 0;
-    for (int i = 0; i < sizeMatriz; i++) {
-        for (int j = 0; j < sizeMatriz; j++) {
-            printf("{%d}",Matriz3[i][j]);
-            c++;
-            if (c == sizeMatriz)
-            {
-                printf("\n");
-                c = 0;
+            // int c = 0;
+            // for (int i = 0; i < sizeMatriz; i++) {
+            //     for (int j = 0; j < sizeMatriz; j++) {
+            //         printf("[%d]", Matriz1[i][j]);
+            //         c++;
+            //         if (c == sizeMatriz) {
+            //             printf("\n");
+            //             c = 0;
+            //         }
+            //     }
+            // }
+
+            // c = 0;
+            // for (int i = 0; i < sizeMatriz; i++) {
+            //     for (int j = 0; j < sizeMatriz; j++) {
+            //         printf("<%d>", Matriz2[i][j]);
+            //         c++;
+            //         if (c == sizeMatriz) {
+            //             printf("\n");
+            //             c = 0;
+            //         }
+            //     }
+            // }
+
+            start = clock();
+            // Multiplicar las matrices
+            multiplicarMatrices(Matriz1, Matriz2, Matriz3, sizeMatriz);
+            end = clock();
+
+            tiempo = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+            // c = 0;
+            // for (int i = 0; i < sizeMatriz; i++) {
+            //     for (int j = 0; j < sizeMatriz; j++) {
+            //         printf("{%d}", Matriz3[i][j]);
+            //         c++;
+            //         if (c == sizeMatriz) {
+            //             printf("\n");
+            //             c = 0;
+            //         }
+            //     }
+            // }
+
+            // Liberar la memoria de las matrices
+            for (int i = 0; i < sizeMatriz; i++) {
+                free(Matriz1[i]);
+                free(Matriz2[i]);
+                free(Matriz3[i]);
             }
-        }
-    }
+            free(Matriz1);
+            free(Matriz2);
+            free(Matriz3);
 
-    tiempo = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Tiempo promedio multiplicacion matrices [%d x %d] = {%.2f minutos}|{%f segundos}|{%f milisegundos}\n", sizeMatriz, sizeMatriz, (tiempo / 10) / 60, tiempo / 10, (tiempo / 10) * 1000);
+            rep = rep - 1;
+            promedio = promedio + tiempo;
 
-    for (int i = 0; i < sizeMatriz; i++) {
-        free(Matriz1[i]);
-        free(Matriz2[i]);
-        free(Matriz3[i]);
-    }
-    free(Matriz1);
-    free(Matriz2);
-    free(Matriz3);
+        } while (rep != 0);
+
+        printf("Tiempo promedio multiplicacion matrices [%d x %d] = {%.2f minutos}|{%f segundos}|{%f milisegundos}\n", sizeMatriz, sizeMatriz, (promedio / 10) / 60, promedio / 10, (promedio / 10) * 1000);
+        guardarTiemposEnArchivo(promedio, "tiempos.txt", sizeMatriz);
+        sizeMatriz = sizeMatriz * 2;
+
+    } while (sizeMatriz <= 512);
 
     return 0;
 }
